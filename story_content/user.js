474fbb92dededab3484b590fa8d87f752e1c8c05
mekarlumongga <152015238+mekarlumongga@users.noreply.github.com>
2025-08-20 21:29:04 +0700

    files upload

---
+++ story_content/user.js
@@ -0,0 +1,119 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6Q7cfrHhgo4":
+        Script1();
+        break;
+      case "6ns19qPwEYZ":
+        Script2();
+        break;
+      case "6dASK75XtBb":
+        Script3();
+        break;
+      case "65zyjHc39Kt":
+        Script4();
+        break;
+      case "6UA18ejCo8z":
+        Script5();
+        break;
+      case "6Vvt3akDfjh":
+        Script6();
+        break;
+      case "5VLInRoVEQs":
+        Script7();
+        break;
+      case "5w4GpM0vHrv":
+        Script8();
+        break;
+      case "6CmbLqhs76B":
+        Script9();
+        break;
+      case "6klHAAL6yEq":
+        Script10();
+        break;
+      case "6h8vlOvkh5e":
+        Script11();
+        break;
+      case "6bt1xb44mP5":
+        Script12();
+        break;
+  }
+}
+
+function Script1()
+{
+  var audio = document.getElementById('bgSongku');
+audio.src="musikku.mp3";
+audio.load();
+audio.play();
+
+}
+
+function Script2()
+{
+  var audio = document.getElementById('bgSongku');
+audio.volume = 0.0;
+}
+
+function Script3()
+{
+  var audio = document.getElementById('bgSongku');
+audio.volume = 0.1;
+}
+
+function Script4()
+{
+  var audio = document.getElementById('bgSongku');
+audio.volume = 0.2;
+}
+
+function Script5()
+{
+  var audio = document.getElementById('bgSongku');
+audio.volume = 0.3;
+}
+
+function Script6()
+{
+  var audio = document.getElementById('bgSongku');
+audio.volume = 0.4;
+
+}
+
+function Script7()
+{
+  var audio = document.getElementById('bgSongku');
+audio.volume = 0.5;
+}
+
+function Script8()
+{
+  var audio = document.getElementById('bgSongku');
+audio.volume = 0.6;
+}
+
+function Script9()
+{
+  var audio = document.getElementById('bgSongku');
+audio.volume = 0.7;
+}
+
+function Script10()
+{
+  var audio = document.getElementById('bgSongku');
+audio.volume = 0.8;
+}
+
+function Script11()
+{
+  var audio = document.getElementById('bgSongku');
+audio.volume = 0.9;
+}
+
+function Script12()
+{
+  var audio = document.getElementById('bgSongku');
+audio.volume = 1.0;
+}
+
