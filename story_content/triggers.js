96474dcba2b68031aaf0a02d2b2296e99cc3fa6d
mekarlumongga <152015238+mekarlumongga@users.noreply.github.com>
2025-08-25 21:43:48 +0700

    file upload

---
+++ story_content/triggers.js
@@ -0,0 +1,59 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "5XXFnszpotk":
+        Script1();
+        break;
+      case "6f8KZzWe8RH":
+        Script2();
+        break;
+      case "5yFbY0KfDeh":
+        Script3();
+        break;
+      case "5ylCdy5TU2C":
+        Script4();
+        break;
+      case "5pzqWmzPH2L":
+        Script5();
+        break;
+      case "6qC6ZRLuNQF":
+        Script6();
+        break;
+      case "5smkaZiu5zH":
+        Script7();
+        break;
+      case "5iS8AXN4Tnq":
+        Script8();
+        break;
+      case "65e3gsJwecO":
+        Script9();
+        break;
+      case "5yxutOpsS5s":
+        Script10();
+        break;
+      case "5VEsoYU0SOi":
+        Script11();
+        break;
+      case "6SisNLnUSLP":
+        Script12();
+        break;
+  }
+}
+
+window.InitExecuteScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var once = player.once;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+var update = player.update;
+var pointerX = player.pointerX;
+var pointerY = player.pointerY;
+var showPointer = player.showPointer;
+var hidePointer = player.hidePointer;
+var slideWidth = player.slideWidth;
+var slideHeight = player.slideHeight;
+};
--- story_content/user.js
+++ story_content/user.js
@@ -1,47 +1,19 @@
-function ExecuteScript(strId)
+window.InitUserScripts = function()
 {
-  switch (strId)
-  {
-      case "5Yz0D4pok04":
-        Script1();
-        break;
-      case "6qlAvQDKq5t":
-        Script2();
-        break;
-      case "6jCVSb8D2EO":
-        Script3();
-        break;
-      case "6VTox5vWj3k":
-        Script4();
-        break;
-      case "6iuEnTejCtG":
-        Script5();
-        break;
-      case "5wwiHy8pQKu":
-        Script6();
-        break;
-      case "5x7DNVPmZSC":
-        Script7();
-        break;
-      case "5vQ2o8kXzys":
-        Script8();
-        break;
-      case "6i1sxMqDj4j":
-        Script9();
-        break;
-      case "6HrFBG8gwwy":
-        Script10();
-        break;
-      case "6hWsodmaZoD":
-        Script11();
-        break;
-      case "5xbxPJKdIUx":
-        Script12();
-        break;
-  }
-}
-
-function Script1()
+var player = GetPlayer();
+var object = player.object;
+var once = player.once;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+var update = player.update;
+var pointerX = player.pointerX;
+var pointerY = player.pointerY;
+var showPointer = player.showPointer;
+var hidePointer = player.hidePointer;
+var slideWidth = player.slideWidth;
+var slideHeight = player.slideHeight;
+window.Script1 = function()
 {
   var audio = document.getElementById('bgSongku');
 audio.src="musikku.mp3";
@@ -50,70 +22,71 @@ audio.play();
 
 }
 
-function Script2()
+window.Script2 = function()
 {
   var audio = document.getElementById('bgSongku');
 audio.volume = 0.0;
 }
 
-function Script3()
+window.Script3 = function()
 {
   var audio = document.getElementById('bgSongku');
 audio.volume = 0.1;
 }
 
-function Script4()
+window.Script4 = function()
 {
   var audio = document.getElementById('bgSongku');
 audio.volume = 0.2;
 }
 
-function Script5()
+window.Script5 = function()
 {
   var audio = document.getElementById('bgSongku');
 audio.volume = 0.3;
 }
 
-function Script6()
+window.Script6 = function()
 {
   var audio = document.getElementById('bgSongku');
 audio.volume = 0.4;
 
 }
 
-function Script7()
+window.Script7 = function()
 {
   var audio = document.getElementById('bgSongku');
 audio.volume = 0.5;
 }
 
-function Script8()
+window.Script8 = function()
 {
   var audio = document.getElementById('bgSongku');
 audio.volume = 0.6;
 }
 
-function Script9()
+window.Script9 = function()
 {
   var audio = document.getElementById('bgSongku');
 audio.volume = 0.7;
 }
 
-function Script10()
+window.Script10 = function()
 {
   var audio = document.getElementById('bgSongku');
 audio.volume = 0.8;
 }
 
-function Script11()
+window.Script11 = function()
 {
   var audio = document.getElementById('bgSongku');
 audio.volume = 0.9;
 }
 
-function Script12()
+window.Script12 = function()
 {
   var audio = document.getElementById('bgSongku');
 audio.volume = 1.0;
 }
 
+};
